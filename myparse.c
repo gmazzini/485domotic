33e462ee07662ef0775bdd36c51d68bfc677060e
Gianluca Mazzini <gianluca@mazzini.org>
2024-10-05 17:24:47 +0200

    Update myparse.c

--- myparse.c
+++ myparse.c
@@ -147,6 +147,11 @@ int main(){
   sock=socket(AF_INET,SOCK_DGRAM,0);
   fromlen=sizeof(from);
 //  fcntl(sock,F_SETFL,O_NONBLOCK);
+  struct sockaddr_in server_addr;
+  server_addr.sin_family=AF_INET;
+  server_addr.sin_port=htons(PORT);
+  server_addr.sin_addr.s_addr=htonl(INADDR_ANY);
+  bind(sock,(struct sockaddr *)&server_addr,sizeof(server_addr);
   
   // receiving events
   for(;;){
