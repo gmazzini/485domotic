92cdd5d261a5411be01bfa45d8eae30d6b72cb7f
Gianluca Mazzini <gianluca@mazzini.org>
2024-10-06 17:06:17 +0200

    Update functions.c

--- functions.c
+++ functions.c
@@ -8,7 +8,7 @@ char * managewww(int sock){
   fromlen=sizeof(from);
   char buf[100],out[2000],*t1,*t2,*f;
   int rr,quit;
-  uint16_t q,y;
+  uint16_t q,j;
   FILE *fp;
   time_t myt;
   struct tm *info;
