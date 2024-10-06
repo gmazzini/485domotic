37c12fa552962e5ce68bffc20db1da17a150087e
Gianluca Mazzini <gianluca@mazzini.org>
2024-10-06 16:43:29 +0200

    Update functions.c

--- functions.c
+++ functions.c
@@ -6,7 +6,7 @@ char * managewww(int sock){
   struct sockaddr from;
   unsigned int fromlen;
   fromlen=sizeof(from);
-  char buf[100],out[1000],*t1,*t2,*f;
+  char buf[100],out[2000],*t1,*t2,*f;
   int rr,quit;
   uint8_t q;
   FILE *fp;
@@ -19,43 +19,46 @@ char * managewww(int sock){
   rr=recvfrom(sock,buf,100,0,&from,&fromlen);
   if(rr<1)return ret;
   *(buf+rr)='\0';
+  sprintf(out,"domotic by GM @2024\n");
+  sprintf(out+strlen(out),">> %s\n",buf);
   t1=strtok(buf," \n\r\t");
   t2=strtok(NULL," \n\r\t");
-  if(strcmp(t1,"time")==0){
+  if(strcmp(t1,"status")==0){
+    sprintf(out+strlen(out),"events: %d\n",nevent);
     time(&myt); info=localtime(&myt); strftime(buf,100,"%d.%m.%Y %H:%M:%S %A",info);
-    sprintf(out,"now: %s\n",buf);
+    sprintf(out+strlen(out),"time now: %s\n",buf);
     info=localtime(&start); strftime(buf,100,"%d.%m.%Y %H:%M:%S %A",info);
-    sprintf(out+strlen(out),"start: %s\n",buf);
-    sprintf(out+strlen(out),"sunrise: %02d:%02d\n",HHr,MMr);
-    sprintf(out+strlen(out),"sunset: %02d:%02d\n",HHs,MMs);
+    sprintf(out+strlen(out),"time start: %s\n",buf);
+    sprintf(out+strlen(out),"time sunrise: %02d:%02d\n",HHr,MMr);
+    sprintf(out+strlen(out),"time sunset: %02d:%02d\n",HHs,MMs);
   }
   else if(strcmp(t1,"seton")==0){
-    sprintf(out,"set relais to on: %s\n",t2);
+    sprintf(out+strlen(out),"set relais to on: %s\n",t2);
     f=strchr(t2,','); *f='\0';
     en=ex; en->R[0]=10*atoi(t2+1)+atoi(f+1); en->nC=1; en->C[0]=2;
     strcpy(ret,"E0");
   }
   else if(strcmp(t1,"setoff")==0){
-    sprintf(out,"set relais to off: %s\n",t2);
+    sprintf(out+strlen(out),"set relais to off: %s\n",t2);
     f=strchr(t2,','); *f='\0';
     en=ex; en->R[0]=10*atoi(t2+1)+atoi(f+1); en->nC=1; en->C[0]=3;
     strcpy(ret,"E0");
   }
   else if(strcmp(t1,"showon")==0){
-    sprintf(out,"show relais on:");
+    sprintf(out+strlen(out),"show relais on:");
     for(q=0;q<TOTRELAIS;q++)if(relais[q]==1)sprintf(out+strlen(out)," R%d,%d",q/10,q%10);
     sprintf(out+strlen(out),"\n");
   }
   else if(strcmp(t1,"inject")==0){
-    sprintf(out,"inject: %s\n",t2);
+    sprintf(out+strlen(out),"inject: %s\n",t2);
     strcpy(ret,t2);
   }
   else if(strcmp(t1,"quit")==0){
-    sprintf(out,"quitting\n");
+    sprintf(out+strlen(out),"quitting\n");
     quit=1;
   }
   else if(strcmp(t1,"help")==0){
-    sprintf(out,"time, show actual time informations\n");
+    sprintf(out+strlen(out),"status, show actual status informations\n");
     sprintf(out+strlen(out),"seton Ri,j, set the relais Ri,j to on\n");
     sprintf(out+strlen(out),"setoff Ri,j, set the relais Ri,j to off\n");
     sprintf(out+strlen(out),"showon, show relais in on state\n");
@@ -63,7 +66,7 @@ char * managewww(int sock){
     sprintf(out+strlen(out),"quit, shutdown the system\n");
     sprintf(out+strlen(out),"help, this help\n");
   }
-  else sprintf(out,"command not find\n");
+  else sprintf(out+strlen(out),"command not find\n");
   sendto(sock,out,strlen(out),0,&from,fromlen);
   if(quit==1){
     fp=fopen(SAVESTATUS,"wb");
