78e1168bb2e25e2e90f63770597e8a2d1d0dd445
Gianluca Mazzini <gianluca@mazzini.org>
2024-10-07 17:47:36 +0200

    Update functions.c

--- functions.c
+++ functions.c
@@ -118,7 +118,7 @@ char * managewww(int sock){
         if(fulllog==0){i=0; j=poslog;}
         else {i=poslog; j=poslog+LOGLEN;} 
       }
-      printf("%d %d %d\n,i,j,poslog);
+      printf("%d %d %d %d\n",i,j,poslog,fulllog);
       for(q=i;q<j;q++){
         info=localtime(&mylog[q%LOGLEN].time); strftime(buf,100,"%d.%m.%Y %H:%M:%S %A",info);
         myout(sock,(q==j-1)?2:1,"%s %d %s\n",buf,mylog[q%LOGLEN].action,mylog[q%LOGLEN].desc); 
