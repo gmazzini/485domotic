7d0c4eba51f0030d9556589179573425d58f2994
Gianluca Mazzini <gianluca@mazzini.org>
2024-11-23 14:31:58 +0100

    Update m_send.c

--- m_send.c
+++ m_send.c
@@ -1,4 +1,4 @@
-#define DEBUG
+#define noDEBUG
 #define SERIAL "/dev/ttyACM1"
 #define HOSTNAME "64.112.124.143"
 #define PORT 44444
@@ -21,24 +21,30 @@ void main(int argc,char **argv){
   servaddr.sin_port=htons(PORT);
 
   switch(mode){
-    case 1:
+    case 1: // 1 v1 v2 v3 i1 i2 i3
       myw(fd,"\x01\x03\x00\x0E",14); 
       of=myr_fn(fd,7);
       if(of==NULL)break;
       *buf=1;
       *((float *)(buf+1))=of[0];
       *((float *)(buf+5))=of[1];
-          *((float *)(buf+1))=of[0];
-
-          *((float *)(buf+1))=of[0];
-
-          *((float *)(buf+1))=of[0];
-
-          *((float *)(buf+1))=of[0];
-
-   
+      *((float *)(buf+9))=of[2];
+      *((float *)(buf+13))=of[4];
+      *((float *)(buf+17))=of[5];
+      *((float *)(buf+21))=of[6];
+      sendto(fd2,buf,25,0,(struct sockaddr *)&servaddr,sizeof(servaddr));
+      break;
+    case 2: // 2 E1 E2 E3
+      myw(fd,"\x01\x03\x01\x02",6); 
+      of=myr_fn(fd,3);
+      if(of==NULL)break;
+      *buf=2;
+      *((float *)(buf+1))=of[0];
+      *((float *)(buf+5))=of[1];
+      *((float *)(buf+9))=of[2];
+      sendto(fd2,buf,13,0,(struct sockaddr *)&servaddr,sizeof(servaddr));
+      break;
   }
-
-    
+  close(fd2); 
   close(fd);
 }
