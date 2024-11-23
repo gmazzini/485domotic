d0a8190bc092860f1cb092e35fb97987a81a9bbd
Gianluca Mazzini <gianluca@mazzini.org>
2024-11-23 14:00:42 +0100

    Update m_test.c

--- m_test.c
+++ m_test.c
@@ -2,17 +2,21 @@
 #define SERIAL "/dev/ttyACM1"
 #include "m_functions.c"
 
-void main(){
-  int fd,ow;
+void main(int argc,char **argv){
+  int fd,ow,mode;
   float *of;
   uint8_t *os;
 
   fd=open(SERIAL,O_RDWR);
   setserial(fd);
+  mode=atoi(argv[1]);
 
-  myw(fd,"\x01\x03\x00\x0E",14); 
-  of=myr_fn(fd,7);
-  if(of!=NULL){
+  switch(mode){
+    case 1:
+      myw(fd,"\x01\x03\x00\x0E",14); 
+      of=myr_fn(fd,7);
+      if(of==NULL)break;
+      
     printf("V1: %6.2f\n",*of);
     printf("V2: %6.2f\n",*(of+1));
     printf("V3: %6.2f\n",*(of+2));
