ba02b2e506d68215e81f5ee9dcb3ad176a7d8f1f
Gianluca Mazzini <gianluca@mazzini.org>
2024-11-23 05:38:48 +0100

    Update m_test.c

--- m_test.c
+++ m_test.c
@@ -10,12 +10,16 @@ void main(){
   fd=open(SERIAL,O_RDWR);
   setserial(fd);
 
-  myw(fd,"\x01\x03\x00\x0E",6); 
-  of=myr_fn(fd,3);
+  myw(fd,"\x01\x03\x00\x0E",14); 
+  of=myr_fn(fd,7);
   if(of!=NULL){
     printf("V1: %6.2f\n",*of);
     printf("V2: %6.2f\n",*(of+1));
     printf("V3: %6.2f\n",*(of+2));
+    printf("HZ: %6.2f\n",*(of+3));
+    printf("I1: %6.2f\n",*(of+4));
+    printf("I2: %6.2f\n",*(of+5));
+    printf("I3: %6.2f\n",*(of+6));
   }
 
     
