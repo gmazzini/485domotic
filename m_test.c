24905fddf61f1c3ac386ea31e363ca80fbeefb95
Gianluca Mazzini <gianluca@mazzini.org>
2024-11-23 05:18:20 +0100

    Create m_test.c

---
+++ m_test.c
@@ -0,0 +1,17 @@
+#define noDEBUG
+#define SERIAL "/dev/ttyACM1"
+#include "m_functions.c"
+
+void main(){
+  int fd,ow;
+  float of;
+  uint8_t *os;
+
+  fd=open(SERIAL,O_RDWR);
+  setserial(fd);
+
+  myw(fd,"\x01\x03\x00\x0E",6); of=myr_f(fd); printf("Tensione 1: %f\n",of);
+
+
+  close(fd);
+}
