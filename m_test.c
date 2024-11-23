4a03f984ca29dde8f3e97d3274a2de7720d1de26
Gianluca Mazzini <gianluca@mazzini.org>
2024-11-23 05:31:27 +0100

    Update m_test.c

--- m_test.c
+++ m_test.c
@@ -12,7 +12,9 @@ void main(){
 
   myw(fd,"\x01\x03\x00\x0E",6); 
   of=myr_fn(fd,3); 
-  printf("V1: %f\n",*of);
+  printf("V1: %6.2f\n",*of);
+  printf("V2: %6.2f\n",*(of+4));
+  printf("V3: %6.2f\n",*(of+8));
 
   close(fd);
 }
