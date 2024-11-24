5b695768bcf67b41deae5a9c9ce571634b9a03f0
Gianluca Mazzini <gianluca@mazzini.org>
2024-11-24 17:06:23 +0100

    Update m_test.c

--- m_test.c
+++ m_test.c
@@ -4,6 +4,7 @@
 
 void main(){
   int fd,ow;
+  long *ol;
   float of;
   uint8_t *os;
 
@@ -11,6 +12,8 @@ void main(){
   setserial(fd);
 
   myw(fd,"\x01\x03\x01\x0A",1); ow=myr_w(fd); printf("Hz: %d\n",ow);
+  myw(fd,"\x01\x03\x01\x00",2); ol=myr_ln(fd,1); printf("V: %ld\n",*ol);
+
 
   close(fd);
 }
