00faf962605cfa889b5dd9eef37d9c4af27439de
Gianluca Mazzini <gianluca@mazzini.org>
2025-02-16 07:18:34 +0100

    Update q2.php

--- q2.php
+++ q2.php
@@ -5,7 +5,7 @@ $message=$argv[1];
 if(isset($argv[2]))$message=$message." ".$argv[2];
 if(isset($argv[3]))$message=$message." ".$argv[3];
 $socket=socket_create(AF_INET,SOCK_DGRAM,SOL_UDP);
-socket_set_option($socket,SOL_SOCKET,SO_RCVTIMEO,array("sec"=>1,"usec"=>0));
+socket_set_option($socket,SOL_SOCKET,SO_RCVTIMEO,array("sec"=>3,"usec"=>0));
 socket_sendto($socket,$message,strlen($message),0,$server_ip,$server_port);
 
 for(;;){
