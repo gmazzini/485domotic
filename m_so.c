50819429c70391d0e72941cc85a5e04fdad06e71
Gianluca Mazzini <gianluca@mazzini.org>
2025-02-25 18:35:03 +0100

    Update m_so.c

--- m_so.c
+++ m_so.c
@@ -61,9 +61,9 @@ void main(int argc,char **argv){
       break;
 
     case 2:
-      qmyw(fd,(uint8_t *)"\x01\x03\x01\x02",2); e1=qmyr_f(fd);
-      qmyw(fd,(uint8_t *)"\x01\x03\x01\x04",2); e2=qmyr_f(fd);
-      qmyw(fd,(uint8_t *)"\x01\x03\x01\x06",2); e3=qmyr_f(fd);
+      myw(fd,(uint8_t *)"\x01\x03\x01\x02",2); e1=qmyr_f(fd);
+      myw(fd,(uint8_t *)"\x01\x03\x01\x04",2); e2=qmyr_f(fd);
+      myw(fd,(uint8_t *)"\x01\x03\x01\x06",2); e3=qmyr_f(fd);
       if(e1>=0 && e2>=0 && e3>=0){
         sprintf(query,"insert into energy_so (epoch,e1,e2,e3) values(%ld,%f,%f,%f)",t,e1,e2,e3);
         mysql_query(con,query);
