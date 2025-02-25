7fe368472f49b2e62d7f9a8a0ae93af7bd7f2da5
Gianluca Mazzini <gianluca@mazzini.org>
2025-02-25 16:46:34 +0100

    Update m_so.c

--- m_so.c
+++ m_so.c
@@ -1,10 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <time.h>
+#include <fcntl.h>
+#include <math.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <termios.h>
 #include <mysql/mysql.h>
-#include <signal.h>
-#include "m_functions.c"
 #include "/home/tools/setup_energy.c"
 
-void handler(){
-  exit(0);
+union uw {uint16_t w; uint8_t u[2]; };
+union uf {float f; uint8_t u[4]; };
+
+uint16_t crc(uint8_t *buf,int lenbuf){
+  int i,j;
+  uint16_t out=0xFFFF;
+  for(j=0;j<lenbuf;j++){
+    out^=(uint16_t)buf[j];
+    for(i=0;i<8;++i){
+      if(out&1)out=(out>>1)^0xA001;
+      else out=(out>>1);
+    }
+  }
+  return out;
+}
+
+float myr_f(int fd){
+  union uw uw;
+  union uf uf;
+  int x,i;
+  static uint8_t aux[9];
+  for(i=0;i<9;i++)read(fd,aux+i,1);
+  uw.u[0]=aux[7]; uw.u[1]=aux[8];
+  if(crc(aux,7)!=uw.w)return -1000;
+  uf.u[3]=aux[3]; uf.u[2]=aux[4]; uf.u[1]=aux[5]; uf.u[0]=aux[6];
+  return uf.f;
+}
+
+void myw(int fd,uint8_t *ss,uint8_t nn){
+  union uw uw;
+  uint8_t aux[8];
+  int i;
+  memcpy(aux,ss,4);
+  aux[4]=0;
+  aux[5]=nn;
+  uw.w=crc(aux,6);
+  aux[6]=uw.u[0];
+  aux[7]=uw.u[1];
+  write(fd,aux,8);
+  usleep(10000);
 }
 
 void main(int argc,char **argv){
@@ -14,15 +62,13 @@ void main(int argc,char **argv){
   MYSQL *con=mysql_init(NULL);
   time_t t;
   char buf[100],query[200];
-
-  // signal(SIGALRM,handler);
-  // alarm(5);
+  
   mode=atoi(argv[1]);
   fd=socket(AF_INET,SOCK_STREAM,0);
   server.sin_family=AF_INET;
   server.sin_port=htons(4196);
   server.sin_addr.s_addr=inet_addr(IP);
-  if(connect(fd,(struct sockaddr *)&server,sizeof(server)))exit(1); 
+  connect(fd,(struct sockaddr *)&server,sizeof(server)); 
   mysql_real_connect(con,"localhost",USER,PASSWORD,DB,0,NULL,0);
   t=time(NULL);
 
