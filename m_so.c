382b233adff63bc928d4f4002edb01008de478fb
Gianluca Mazzini <gianluca@mazzini.org>
2025-02-25 19:09:53 +0100

    Update m_so.c

--- m_so.c
+++ m_so.c
@@ -2,33 +2,6 @@
 #include "m_functions.c"
 #include "/home/tools/setup_energy.c"
 
-void qmyw(int fd,uint8_t *ss,uint8_t nn){
-  union uw uw;
-  uint8_t aux[8];
-  int i;
-  memcpy(aux,ss,4);
-  aux[4]=0;
-  aux[5]=nn;
-  uw.w=crc(aux,6);
-  aux[6]=uw.u[0];
-  aux[7]=uw.u[1];
-  write(fd,aux,8);
-  usleep(10000);
-}
-
-
-float qmyr_f(int fd){
-  union uw uw;
-  union uf uf;
-  int x,i;
-  static uint8_t aux[9];
-  for(i=0;i<9;i++)read(fd,aux+i,1);
-  uw.u[0]=aux[7]; uw.u[1]=aux[8];
-  // if(crc(aux,7)!=uw.w)return -1000;
-  uf.u[3]=aux[3]; uf.u[2]=aux[4]; uf.u[1]=aux[5]; uf.u[0]=aux[6];
-  return uf.f;
-}
-
 void main(int argc,char **argv){
   int fd,ow,mode;
   float v1,v2,v3,i1,i2,i3,e1,e2,e3;
@@ -64,7 +37,6 @@ void main(int argc,char **argv){
       myw(fd,(uint8_t *)"\x01\x03\x01\x02",2); e1=myr_f(fd);
       myw(fd,(uint8_t *)"\x01\x03\x01\x04",2); e2=myr_f(fd);
       myw(fd,(uint8_t *)"\x01\x03\x01\x06",2); e3=myr_f(fd);
-      printf("%f %f %f\n",e1,e2,e3);
       if(e1>=0 && e2>=0 && e3>=0){
         sprintf(query,"insert into energy_so (epoch,e1,e2,e3) values(%ld,%f,%f,%f)",t,e1,e2,e3);
         mysql_query(con,query);
