84c1be5171226aefc7eda6971deea56b70bc43a4
Gianluca Mazzini <gianluca@mazzini.org>
2025-10-07 00:11:44 +0200

    Update m_recv.c

--- m_recv.c
+++ m_recv.c
@@ -3,14 +3,14 @@
 #include "/home/tools/setup_energy.c"
 
 int main(int argc,char **argv){
-  const char *host[]={"",IPCC,IPCC,IPCC,IPSORC},*port="80";
+  const char *host[]={"",IPCC,IPCC,IPCC,IPSORC,IPFGM},*port="80";
   struct addrinfo h={0},*r;
   char path[30],buf[1024],*aa,query[200];
   ssize_t n;
   time_t t;
   float v[6];
   MYSQL *con=mysql_init(NULL);
-  int ii;
+  int ii,jj;
 
   ii=atoi(argv[1]);
   sprintf(path,"/cgi-bin/m_read?%d",ii);
@@ -26,29 +26,41 @@ int main(int argc,char **argv){
   mysql_real_connect(con,"localhost",USER,PASSWORD,DB,0,NULL,0);
   t=time(NULL);
   while(poll(&p,1,2000)>0 && (n=read(s,buf,sizeof buf))>0){
-    aa=strstr(buf,"\r\n1,");
-    if(aa!=NULL && sscanf(aa,"\r\n1,%f,%f,%f,%f,%f,%f",&v[0],&v[1],&v[2],&v[3],&v[4],&v[5])==6){
-      sprintf(query,"insert into vi_cc (epoch,v1,v2,v3,i1,i2,i3) values(%ld,%f,%f,%f,%f,%f,%f)",t,v[0],v[1],v[2],v[3],v[4],v[5]);
-      mysql_query(con,query);
-      continue;
-    }
-    aa=strstr(buf,"\r\n2,");
-    if(aa!=NULL && sscanf(aa,"\r\n2,%f,%f,%f",&v[0],&v[1],&v[2])==3){
-      sprintf(query,"insert into energy_cc (epoch,e1,e2,e3) values(%ld,%f,%f,%f)",t,v[0],v[1],v[2]);
-      mysql_query(con,query);
-      continue;
-    }
-    aa=strstr(buf,"\r\n3,");
-    if(aa!=NULL && sscanf(aa,"\r\n3,%f",&v[0])==1){
-      sprintf(query,"insert into energy_le1 (epoch,e) values(%ld,%f)",t,v[0]);
-      mysql_query(con,query);
-      continue;
-    }
-    aa=strstr(buf,"\r\n4,");
-    if(aa!=NULL && sscanf(aa,"\r\n4,%f",&v[0])==1){
-      sprintf(query,"insert into temp_srso (epoch,t) values(%ld,%f)",t,v[0]);
-      mysql_query(con,query);
-      continue;
+    aa=strstr(buf,"\r\n\r\n");
+    if(aa==NULL)continue;
+    if(sscanf(aa+4,"%d,",&jj)!=1)continue;
+    aa=strstr(aa+4,",")+1;
+    switch(ii){
+      case 1:
+        if(jj==1 && sscanf(aa,"%f,%f,%f,%f,%f,%f",&v[0],&v[1],&v[2],&v[3],&v[4],&v[5])==6){
+          sprintf(query,"insert into vi_cc (epoch,v1,v2,v3,i1,i2,i3) values(%ld,%f,%f,%f,%f,%f,%f)",t,v[0],v[1],v[2],v[3],v[4],v[5]);
+          mysql_query(con,query);
+        }
+        break;
+      case 2:
+        if(jj==2 && sscanf(aa,"%f,%f,%f",&v[0],&v[1],&v[2])==3){
+          sprintf(query,"insert into energy_cc (epoch,e1,e2,e3) values(%ld,%f,%f,%f)",t,v[0],v[1],v[2]);
+          mysql_query(con,query);
+        }
+        break;
+      case 3:
+        if(jj==3 && sscanf(aa,"%f",&v[0])==1){
+          sprintf(query,"insert into energy_le1 (epoch,e) values(%ld,%f)",t,v[0]);
+          mysql_query(con,query);
+        }
+        break;
+      case 4:
+        if(jj==4 && sscanf(aa,"%f",&v[0])==1){
+          sprintf(query,"insert into temp_srso (epoch,t) values(%ld,%f)",t,v[0]);
+          mysql_query(con,query);
+        }
+        break;
+      case 5:
+        if(jj==4 && sscanf(aa,"%f",&v[0])==1){
+          sprintf(query,"insert into temp_fgm (epoch,t) values(%ld,%f)",t,v[0]);
+          mysql_query(con,query);
+        }
+        break;
     }
   }
   mysql_close(con);
