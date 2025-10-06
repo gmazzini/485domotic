bbb98da4c8c85a63741331d09fcd6f58f2a7e166
Gianluca Mazzini <gianluca@mazzini.org>
2025-10-06 17:17:51 +0200

    Update and rename w_send.c to water.c

---
+++ water.c
@@ -0,0 +1,31 @@
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <string.h>
+#include <inttypes.h>
+#define PIN_IN 2
+
+uint64_t cwater;
+void iwater(void){
+  cwater++;
+}
+
+int main(void){
+  FILE *fp;
+  char buf[100];
+
+  wiringPiSetup();
+  pinMode(PIN_IN,INPUT);
+  wiringPiISR(PIN_IN,INT_EDGE_RISING,&iwater);
+  fp=fopen(FILE,"r");
+  fscanf(fp,"%" SCNu64,&cwater);
+  fclose(fp);
+  for(;;){
+    sleep(10);
+    fp=fopen(FILE,"w");
+    fprintf(fp,"%" PRIu64 "\n",cwater);
+    fclose(fp);
+  }
+}
