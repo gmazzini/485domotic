017fc016fd5f0b40ef847c6df9a6b6a1dd9aa863
Gianluca Mazzini <gianluca@mazzini.org>
2025-10-06 16:53:59 +0200

    Update m_read.c

--- m_read.c
+++ m_read.c
@@ -12,7 +12,8 @@ int main(void){
   printf("Content-Type: text/plain\n\n");
   switch(atoi(query)){
     case 1: // 1 517 CC v1 v2 v3 i1 i2 i3
-      fd=open(SERIAL,O_RDWR); setserial(fd,'e');
+      fd=open(SERIAL,O_RDWR); if(fd==-1)return 1;
+      setserial(fd,'e');
       myw(fd,"\x01\x03\x00\x0E",14); 
       of=myr_fn(fd,7);
       close(fd);
@@ -20,7 +21,8 @@ int main(void){
       printf("1,%f,%f,%f,%f,%f,%f\n",of[0],of[1],of[2],of[4],of[5],of[6]);
       break;
     case 2: // 2 517 CC E1 E2 E3
-      fd=open(SERIAL,O_RDWR); setserial(fd,'e');
+      fd=open(SERIAL,O_RDWR); if(fd==-1)return 1;
+      setserial(fd,'e');
       myw(fd,"\x01\x03\x01\x02",6); 
       of=myr_fn(fd,3);
       close(fd);
@@ -28,7 +30,8 @@ int main(void){
       printf("2,%f,%f,%f\n",of[0],of[1],of[2]);
       break;
     case 3: // 3 526 CC E
-      fd=open(SERIAL,O_RDWR); setserial(fd,'n');
+      fd=open(SERIAL,O_RDWR); if(fd==-1)return 1;
+      setserial(fd,'n');
       myw(fd,"\x01\x03\x01\x0E",2);
       ol=myr_ln(fd,1);
       close(fd);
