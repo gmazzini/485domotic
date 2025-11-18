3be6b61a4a2b17d73fcd8c9ab7540278d3399aeb
Gianluca Mazzini <gianluca@mazzini.org>
2025-11-18 22:20:52 +0100

    Update m_functions.c

--- m_functions.c
+++ m_functions.c
@@ -21,7 +21,7 @@ union uw {uint16_t w; uint8_t u[2]; };
 union ul {uint32_t l; uint8_t u[4]; };
 union uf {float f; uint8_t u[4]; };
 
-void setserial(int fd,char p){
+void NOOOOsetserial(int fd,char p){
   struct termios tty;
   memset(&tty,0,sizeof(tty));
   if(tcgetattr(fd,&tty)!=0)return;
@@ -49,7 +49,7 @@ void setserial(int fd,char p){
   tcsetattr(fd,TCSANOW,&tty);
 }
 
-void OLDsetserial(int fd,char p){
+void setserial(int fd,char p){
   struct termios tty;
   memset(&tty,0,sizeof(tty));
   tcgetattr(fd,&tty);
