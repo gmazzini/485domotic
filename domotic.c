15f4c3ceeb317ba469f2f2fd84daf9207fe7d068
Gianluca Mazzini <gianluca@mazzini.org>
2026-06-11 12:25:32 +0200

    Update domotic.c

--- domotic.c
+++ domotic.c
@@ -69,7 +69,8 @@ struct kmap kmap[MAXKMAP];
 uint16_t knownrelais[MAXKNOWNRELAIS];
 uint8_t HHr,MMr,HHs,MMs,fulllog;
 uint16_t nevent,poslog,totwhite,nkmap,nknownrelais;
-time_t start;
+time_t start,last_reload;
+uint16_t last_reload_error;
 uint64_t mask[64];
 struct acl{
   struct in6_addr addr;
@@ -81,191 +82,29 @@ struct acl white[TOTWHITE];
 
 int main(){
   FILE *fp;
-  char buf[100],code[4],label[LABELLEN];
+  char buf[100],code[4];
   uint8_t modS[MAXEVENTRELAIS];
-  char *token,*f,*mye;
+  char *mye;
   time_t myt;
   struct tm info;
-  uint16_t i,j,q,*lK,nlK,*lR,nlR,*lE,nlE,nlC,*lC,slC,*lT,nlT,nlS,*lSe,*lSt;
-  uint16_t last_min,last_hour,sched,every10,every30,esun,event,nmod,*modR,relay,key;
-  uint64_t *lD;
-  struct ek *en,*em;
+  uint16_t j,q,last_min,last_hour,sched,every10,every30,esun,event,nmod,*modR,relay,errline;
+  struct ek *en;
   struct es *es;
   int sockwww,state,oncount,ok,before;
   struct sockaddr_in6 server_addr;
 
-  for(q=0;q<64;q++)mask[q]=1ULL<<q;
-
   ee=(struct ek *)malloc(TOTEK*sizeof(struct ek));
   ex=(struct ek *)malloc(TOTEX*sizeof(struct ek));
   es=NULL;
-  nkmap=0;
-  nknownrelais=0;
 
-  for(q=0;q<TOTEK;q++)ee[q].event=0;
-  for(q=0;q<TOTEX;q++)ex[q].event=0;
+  memset(ee,0,TOTEK*sizeof(struct ek));
+  memset(ex,0,TOTEX*sizeof(struct ek));
 
-  lK=(uint16_t *)malloc(100*sizeof(uint16_t));
-  lR=(uint16_t *)malloc(100*sizeof(uint16_t));
-  lE=(uint16_t *)malloc(100*sizeof(uint16_t));
-  lC=(uint16_t *)malloc(100*sizeof(uint16_t));
-  lD=(uint64_t *)malloc(23*sizeof(uint64_t));
-  lT=(uint16_t *)malloc(100*sizeof(uint16_t));
-  lSe=(uint16_t *)malloc(100*sizeof(uint16_t));
-  lSt=(uint16_t *)malloc(100*sizeof(uint16_t));
+  for(q=0;q<64;q++)mask[q]=1ULL<<q;
+
+  errline=load_config();
+
   modR=(uint16_t *)malloc(MAXEVENTRELAIS*sizeof(uint16_t));
-
-  fp=fopen(CONFIG,"r");
-  nevent=1;
-
-  if(fp!=NULL){
-    for(;;){
-      if(fgets(buf,100,fp)==NULL)break;
-      if(strlen(buf)<5)continue;
-      if(buf[0]=='#')continue;
-
-      if(is_kmap_line(buf)){
-        load_kmap_line(buf);
-        continue;
-      }
-
-      if(is_rrange_line(buf)){
-        load_rrange_line(buf);
-        continue;
-      }
-
-      nlK=0;
-      nlR=0;
-      nlE=0;
-      nlC=0;
-      nlT=0;
-      nlS=0;
-      label[0]='\0';
-
-      for(q=0;q<23;q++)lD[q]=0;
-
-      for(token=strtok(buf," \n\r\t");token;token=strtok(NULL," \n\r\t")){
-        switch(token[0]){
-          case 'K':
-            if(parse_key(token+1,&key))lK[nlK++]=key;
-            break;
-
-          case 'R':
-            if(parse_relais(token+1,&relay))lR[nlR++]=relay;
-            break;
-
-          case 'E':
-            lE[nlE++]=atoi(token+1);
-            break;
-
-          case 'D':
-            f=strchr(token,',');
-            if(f!=NULL){
-              *f='\0';
-              i=atoi(token+3);
-              *(token+3)='\0';
-              i+=atoi(token+1)*60;
-              j=atoi(f+3);
-              *(f+3)='\0';
-              j+=atoi(f+1)*60;
-              for(q=i;q<=j;q++)lD[q/64]|=mask[q%64];
-            }
-            break;
-
-          case 'C':
-            slC=0;
-            for(q=1;q<=7;q++){
-              if(strcmp(token+1,cmd[q])==0){
-                slC=q;
-                break;
-              }
-            }
-            if(slC>0)lC[nlC++]=slC;
-            break;
-
-          case 'T':
-            f=strchr(token,',');
-            if(f!=NULL){
-              *f='\0';
-              if(parse_relais(token+1,&relay)){
-                lT[nlT++]=relay+1000*atoi(f+1);
-              }
-            }
-            break;
-
-          case 'S':
-            f=strchr(token,',');
-            if(f!=NULL){
-              *f='\0';
-              lSe[nlS]=atoi(token+1);
-              lSt[nlS++]=atoi(f+1);
-            }
-            break;
-
-          case 'L':
-            strncpy(label,token+1,LABELLEN-1);
-            label[LABELLEN-1]='\0';
-            break;
-        }
-      }
-
-      for(q=0;q<nlK+nlE;q++){
-        if(q<nlK)en=ee+lK[q];
-        else en=ex+lE[q-nlK];
-
-        if(en->event>0){
-          for(;en->next!=NULL;en=en->next);
-          em=(struct ek *)malloc(sizeof(struct ek));
-          en->next=em;
-          en=em;
-        }
-
-        en->event=nevent++;
-
-        en->nR=nlR;
-        en->R=(uint16_t *)malloc(nlR*sizeof(uint16_t));
-        for(j=0;j<nlR;j++)en->R[j]=lR[j];
-
-        en->nC=nlC;
-        en->C=(uint16_t *)malloc(nlC*sizeof(uint16_t));
-        for(j=0;j<nlC;j++)en->C[j]=lC[j];
-
-        en->D=(uint64_t *)malloc(23*sizeof(uint64_t));
-        for(j=0;j<23;j++)en->D[j]=lD[j];
-
-        en->nT=nlT;
-        en->T=(uint16_t *)malloc(nlT*sizeof(uint16_t));
-        for(j=0;j<nlT;j++)en->T[j]=lT[j];
-
-        en->nS=nlS;
-        en->Se=(uint16_t *)malloc(nlS*sizeof(uint16_t));
-        en->St=(uint16_t *)malloc(nlS*sizeof(uint16_t));
-        for(j=0;j<nlS;j++){
-          en->Se[j]=lSe[j];
-          en->St[j]=lSt[j];
-        }
-
-        strncpy(en->label,label,LABELLEN-1);
-        en->label[LABELLEN-1]='\0';
-
-        en->next=NULL;
-      }
-    }
-
-    fclose(fp);
-  }
-
-  sort_kmap();
-  sort_known_relais();
-
-  free(lK);
-  free(lR);
-  free(lE);
-  free(lC);
-  free(lD);
-  free(lT);
-  free(lSe);
-  free(lSt);
 
   time(&start);
 
@@ -288,24 +127,6 @@ int main(){
   esun=0;
 
   sun(1900+info.tm_year,1+info.tm_mon,info.tm_mday,LAT,LNG,&HHr,&MMr,&HHs,&MMs);
-
-  en=ex;
-  en->event=nevent;
-  en->nR=1;
-  en->R=(uint16_t *)malloc(sizeof(uint16_t));
-  en->R[0]=0;
-  en->nC=1;
-  en->C=(uint16_t *)malloc(sizeof(uint16_t));
-  en->C[0]=0;
-  en->D=(uint64_t *)malloc(23*sizeof(uint64_t));
-  for(q=0;q<23;q++)en->D[q]=0;
-  en->nT=0;
-  en->T=NULL;
-  en->nS=0;
-  en->Se=NULL;
-  en->St=NULL;
-  en->label[0]='\0';
-  en->next=NULL;
 
   mylog=(struct log *)malloc(LOGLEN*sizeof(struct log));
 
@@ -339,6 +160,24 @@ int main(){
     memcpy(&info,localtime(&myt),sizeof(struct tm));
 
     mye=managewww(sockwww);
+
+    if(strcmp(mye,"RELOAD")==0){
+      clear_pending_events(&es);
+      clear_config();
+      errline=load_config();
+
+      if(errline==0){
+        myout(sockwww,2,"config loaded\n");
+        inslog(myt,1,"reload ok");
+      }
+      else {
+        myout(sockwww,2,"config error at line %d\n",errline);
+        sprintf(buf,"reload %d",errline);
+        inslog(myt,1,buf);
+      }
+
+      continue;
+    }
 
     if(strlen(mye)>0){
       strcpy(buf,mye);
